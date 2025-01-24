2a312fd466b4cba551c271bcf6d3c4b9e8cfea92
PickleNik <31113245+PickleNik@users.noreply.github.com>
2025-01-24 12:38:20 -0500

    Update app.py

--- fall2024/inclass/week2/flaskapp/app.py
+++ fall2024/inclass/week2/flaskapp/app.py
@@ -2,21 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-posts = [
-    {
-        'user': 'elonmusk',
-        'text': 'The sun is a thermonuclear explosion fyi'
-    },
-    {
-        'user': 'john',
-        'text': 'Excited for school!!!!'
-    }
-]
-
 @app.route('/')
-@app.route('/index')
 def index_route():
     return render_template('index.html', posts=posts)
+
+posts = [
+    {
+        'user': 'chuck',
+        'text': 'meow'
+    },
+    {
+        'user': 'nikita',
+        'text': 'meow'
+    }
+]
 
 @app.route('/user/<user>')
 def user_route(user):
